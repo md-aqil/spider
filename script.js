5f7d183c4627079cd869eedb65e9e5c76390e14a
Md Adil <md-adil@live.com>
2022-01-03 19:01:42 +0530

    fixes

--- script.js
+++ script.js
@@ -1,37 +1,35 @@
 gsap.registerPlugin(MotionPathPlugin);
-
 const logo = "#dpad-logo";
-function registerPaths(timeline, paths) {
-  paths.split(",").forEach((path) => {
+function registerPath(timeline, path, isLast) {
+    const el = document.querySelector(path);
+    const start = parseInt(el.getAttribute("data-start") || 1);
+    const end = parseInt(el.getAttribute("data-end") || 0);
+    console.log({ start, end });
     timeline.to(logo, {
-      motionPath: {
-        path,
-        align: path,
-        alignOrigin: [0.5, 0.5],
-        autoRotate: false,
-        start: 1,
-        end: 0,
-      },
-      duration: 0.5,
-      scale: 0.5,
-      immediateRender: true,
-      opacity: 0.5,
+        motionPath: {
+            path,
+            align: path,
+            alignOrigin: [0.5, 0.5],
+            start,
+            end,
+        },
+        duration: 0.5,
+        scale: isLast ? 0.5 : 1,
+        immediateRender: true,
+        opacity: 0.5,
     });
-  });
 }
 
 for (const component of document.querySelectorAll("[data-path]")) {
-  const path = component.getAttribute("data-path");
-  const timeline = gsap.timeline({
-    yoyo: true,
-    paused: true,
-    ease: Linear.easeNone,
-  });
-  registerPaths(timeline, path);
-  component.addEventListener("mouseenter", () => {
-    console.log("mouse enter");
-    console.log({ path, timeline });
-    timeline.play();
-  });
-  component.addEventListener("mouseleave", () => timeline.reverse());
+    const paths = component.getAttribute("data-path");
+    const timeline = gsap.timeline({
+        yoyo: true,
+        paused: true,
+        ease: Linear.easeNone,
+    });
+    paths.split(",").map((x, i, all) => registerPath(timeline, x, all.length - 1 === i));
+    component.addEventListener("mouseenter", () => {
+        timeline.play();
+    });
+    component.addEventListener("mouseleave", () => timeline.reverse());
 }
